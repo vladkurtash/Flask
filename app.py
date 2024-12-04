6f7209c56e3d1c71fe763af5335ae20824ab56d5
vladkurtash <er.vladkurtash@gmail.com>
2024-12-05 00:58:23 +0200

    Add Main Files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
